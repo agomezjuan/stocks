201d314d29b1e6fdc1d753d3d78ae1a8cfa44450
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-11-14 21:58:38 +0100

    feat: edit product form

---
+++ src/pages/Edit.jsx
@@ -0,0 +1,11 @@
+import EditProduct from "../components/EditProduct/EditProduct";
+
+const Edit = () => {
+  return (
+    <div>
+      <h1>Editar producto</h1>
+      <EditProduct />
+    </div>
+  );
+};
+export default Edit;
