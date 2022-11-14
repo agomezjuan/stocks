da71a38bc74c62771bebf21e0a0f030eca328ef1
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-11-14 21:59:45 +0100

    feat: create route edit product

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import Edit from "./pages/Edit";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/edit/:id" element={<Edit />} />
         </Route>
         <Route path="*" element={<h1>404: Not Found</h1>} />
       </Routes>
