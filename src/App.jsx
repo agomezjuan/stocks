d0b75a7a27ad78f9121895ce001b40c0bf801415
Alberto Gómez Juan <agomezjuan@hotmail.com>
2022-11-14 18:58:46 +0100

    feat: redux provider

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,20 @@ import Layout from "./components/Layout/Layout";
 import Products from "./pages/Products";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/products" element={<Products />} />
-      </Route>
-      <Route path="*" element={<h1>404: Not Found</h1>} />
-    </Routes>
-    // <div className="App">
-    //   <Header />
-    //   <Footer />
-    // </div>
+    <Provider store={store}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Route>
+        <Route path="*" element={<h1>404: Not Found</h1>} />
+      </Routes>
+    </Provider>
   );
 }
 
